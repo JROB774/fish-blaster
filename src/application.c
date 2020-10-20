2fedae889edaf446d5d25b4b291681c493583fef
Joshua Robertson <jrobertson774@yahoo.com>
2020-10-20 22:50:30 +0100

    Pulled out game code into its own functions.

--- src/application.c
+++ src/application.c
@@ -1,5 +1,7 @@
 #define GAME_BACKGROUND_COLOR 0xFF2F4B99
 
+// HELPER FUNCTIONS
+
 INTERNAL void render_cursor ()
 {
     int x = get_mouse_x()-(SPR_CURSOR_0.w/2);
@@ -22,6 +24,33 @@ INTERNAL void render_score ()
     render_text(x,y, PAL_TEXT_SHADE, SCORE_TEXT, gScore);
 }
 
+// GAME
+
+INTERNAL void update_game (float dt)
+{
+    update_spawner(dt);
+    update_fish(dt);
+
+    // Handle shooting.
+    if (button_pressed(LMB))
+    {
+        // Check for collision with shootable enemies.
+        shake_camera(1,1,0.1f);
+        collide_fish();
+    }
+}
+INTERNAL void render_game (float dt)
+{
+    begin_camera();
+    render_fish(dt);
+    end_camera();
+
+    render_cursor();
+    render_score();
+}
+
+// APPLICATION
+
 INTERNAL bool init_application ()
 {
     seed_random();
@@ -38,25 +67,11 @@ INTERNAL void quit_application ()
 INTERNAL void update_application (float dt)
 {
     update_camera(dt);
-    update_spawner(dt);
-    update_fish(dt);
-
-    // Handle shooting.
-    if (button_pressed(LMB))
-    {
-        shake_camera(1,1,0.05f);
-        collide_fish();
-    }
+    update_game(dt);
 }
 
 INTERNAL void render_application (float dt)
 {
     render_clear(GAME_BACKGROUND_COLOR);
-
-    begin_camera();
-    render_fish(dt);
-    end_camera();
-
-    render_cursor();
-    render_score();
+    render_game(dt);
 }
