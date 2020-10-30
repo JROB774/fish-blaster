6cab61149f9408f4ebdafd7b5f46534213f4680e
Joshua Robertson <jrobertson774@yahoo.com>
2020-10-30 11:48:04 +0000

    Added an icon to the executable.

--- buildwin32.bat
+++ buildwin32.bat
@@ -4,6 +4,6 @@ set DebugFlags=-D BUILD_DEBUG -g
 
 :: Build both the 32-bit and 64-bit versions of the application.
 if not exist bin\win32\x64 mkdir bin\win32\x64
-tcc -I src -L lib\win32 -lSDL2 -lSDL2_mixer -D BUILD_64BIT -bench %DebugFlags% -o bin\win32\x64\FISH.exe src\main_win32.c
+tcc -I src -L lib\win32 -lSDL2 -lSDL2_mixer -D BUILD_64BIT -bench %DebugFlags% -o bin\win32\x64\FISH.exe src\main_win32.c res\icon64.res
 if not exist bin\win32\x86 mkdir bin\win32\x86
-i386-win32-tcc -I src -L lib\win32 -lSDL2 -lSDL2_mixer -D BUILD_32BIT -bench %DebugFlags% -o bin\win32\x86\FISH.exe src\main_win32.c
+i386-win32-tcc -I src -L lib\win32 -lSDL2 -lSDL2_mixer -D BUILD_32BIT -bench %DebugFlags% -o bin\win32\x86\FISH.exe src\main_win32.c res\icon32.res
