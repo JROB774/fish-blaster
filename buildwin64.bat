a336b7c51395afef913325f791390ccf84ebdf5b
Joshua Robertson <joshrobertson774@gmail.com>
2022-01-15 00:45:48 +0000

    Made separate scripts for x64 and x86 compilation.

--- buildwin64.bat
+++ buildwin64.bat
@@ -8,5 +8,5 @@ set ExtraFlags=
 if %BuildMode% == debug set ExtraFlags=%ExtraFlags% -D BUILD_DEBUG -g
 if %BuildMode% == release set ExtraFlags=%ExtraFlags% -Wl,-subsystem=windows
 
-if not exist bin\win32\x86 mkdir bin\win32\x86
-tcc\tcc -I src -L lib\win32 -lSDL2 -lSDL2_mixer -bench -m32 -D BUILD_32BIT %ExtraFlags% -o bin\win32\x86\FISH.exe src\main_win32.c res\iconx86.res
+if not exist bin\win32\x64 mkdir bin\win32\x64
+tcc\tcc -I src -L lib\win32 -lSDL2 -lSDL2_mixer -bench -m64 -D BUILD_64BIT %ExtraFlags% -o bin\win32\x64\FISH.exe src\main_win32.c res\iconx64.res
