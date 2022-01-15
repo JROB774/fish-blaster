80a270b765d103555f40ce67ae24fc0e6bc7df2b
Joshua Robertson <joshrobertson774@gmail.com>
2022-01-15 00:30:22 +0000

    Removed console window from release builds.

--- buildwin32.bat
+++ buildwin32.bat
@@ -11,6 +11,7 @@ if %Architecture% == x86 set ExtraFlags=%ExtraFlags% -m32 -D BUILD_32BIT
 if %Architecture% == x64 set ExtraFlags=%ExtraFlags% -m64 -D BUILD_64BIT
 
 if %BuildMode% == debug set ExtraFlags=%ExtraFlags% -D BUILD_DEBUG -g
+if %BuildMode% == release set ExtraFlags=%ExtraFlags% -Wl,-subsystem=windows
 
 if not exist bin\win32\%Architecture% mkdir bin\win32\%Architecture%
 tcc\tcc -I src -L lib\win32 -lSDL2 -lSDL2_mixer -bench %ExtraFlags% -o bin\win32\%Architecture%\FISH.exe src\main_win32.c res\icon%Architecture%.res
